1b468c1328593d604b2261a9a756a90449ebebbf
358-SMC <s2322069@cco.kanagawa-it.ac.jp>
2025-05-03 00:23:44 +0900

    [CHANGE]Added display of acquisition message
    Freeze occurs when acquiring information, making it difficult to
    understand.

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ async function fetchWebInfo() {
         return;
     }
 
+    message.textContent = '取得しています...';
+    message.style.color = 'black';
+    bottons.forEach(btn => btn.disabled = true); //ボタン無効化
+
     try {
         const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`);
         const data = await response.json();
